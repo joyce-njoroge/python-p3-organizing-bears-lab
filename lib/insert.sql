3132d8bc4b39d572f94f50812d6b3d7f33d1c28f
Joyce Njoroge <joycewambuku33@gmail.com>
2023-05-29 00:25:39 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES 
+(1, 'Mr. Chocolate', 10, 'F', 'Brown', 'social', 1),
+(2, 'Rowdy', 11, 'M', 'Brown', 'social', 0),
+(3, 'Tabitha', 9, 'F', 'Black', 'optimistic', 1),
+(4, 'Sergeant Brown', 15, 'M', 'White', 'irritable', 1),
+(5, 'Melissa', 10, 'F', 'Brown', 'quiet', 0),
+(6, 'Grinch', 12, 'M', 'Black', 'peaceful', 1),
+(7, 'Wendy', 10, 'F', 'Brown', 'analytical', 1),
+(8, NULL, 5, 'M', 'Brown', 'social', 1);
+
+
